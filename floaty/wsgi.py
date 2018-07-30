8ae811022c9a54745122378bbfaba0ffce7d0529
jgfc97 <jg.fc97@gmail.com>
2018-07-30 09:45:55 +0100

    name change

--- floaty/wsgi.py
+++ floaty/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buoy2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "floaty.settings")
 
 application = get_wsgi_application()
